1ac3b21c7ec21c3e44aa8582a7e72cdf9fd1e44b
dekoms <dekoms@yu.ac.kr>
2023-05-08 15:52:32 +0900

    올라갑니다

---
+++ 과제2_StdMng/Student.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, id, name, age, major, grade):
+        self.__id  = id
+        self.__name = name
+        self.__age = age
+        self.__major = major
+        self.__grade = grade
+    def __create__(self):
+        self.__id  = id
+    def __read__(self):
+        self.__id  = id
+    def __update__(self):
+        self.__id  = id
+    def __delete__(self):
+        self.__id  = id
---
+++ 과제2_StdMng/StudentManageImpl.py
@@ -0,0 +1,6 @@
+from abc import *
+import StudentManageRepo
+
+class StudentManageImpl(StudentManageRepo):
+    def __init__(self):
+        self.__id  = id
